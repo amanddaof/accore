eb1e7605005204d01292d0600cd6e242b421f930
amanddaof <88158445+amanddaof@users.noreply.github.com>
2025-12-26 09:24:39 -0300

    Update Footer.jsx

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -1,10 +1,14 @@
 export default function Footer() {
   return (
     <footer className="app-footer">
-      <span>ACCORE</span>
-      <span>•</span>
-      <span>v2.0</span>
+      <div className="footer-title">
+        AC•CORE <span>v2.0</span>
+      </div>
+
+      <div className="footer-desc">
+        Painel de controle financeiro desenvolvido por Amanda Oliveira —
+        Machado/MG — dezembro, 2025
+      </div>
     </footer>
   );
 }
-
