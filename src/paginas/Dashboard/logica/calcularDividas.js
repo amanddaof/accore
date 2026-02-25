0ed950b81dfccc1c9c15d942c3b9a9be9c6c8599
amanddaof <88158445+amanddaof@users.noreply.github.com>
2026-02-25 13:52:56 -0300

    Update calcularDividas.js

--- src/paginas/Dashboard/logica/calcularDividas.js
+++ src/paginas/Dashboard/logica/calcularDividas.js
@@ -132,30 +132,31 @@ export function calcularDividas({
   });
 
   /* =========================
-     4️⃣ RESULTADO FINAL
-  ========================= */
+   4️⃣ RESULTADO FINAL
+========================= */
 
-  let saldoFinal;
+let saldoFinal = {
+  quemDeve: null,
+  quemRecebe: null,
+  valor: 0
+};
 
-  if (saldo > 0) {
-    saldoFinal = {
-      quemDeve: "Amanda",
-      valor: saldo
-    };
-  } else if (saldo < 0) {
-    saldoFinal = {
-      quemDeve: "Celso",
-      valor: Math.abs(saldo)
-    };
-  } else {
-    saldoFinal = {
-      quemDeve: null,
-      valor: 0
-    };
-  }
-
-  return {
-    saldoFinal,
-    detalhamento
+if (saldo > 0) {
+  saldoFinal = {
+    quemDeve: "Amanda",
+    quemRecebe: "Celso",
+    valor: saldo
   };
+} else if (saldo < 0) {
+  saldoFinal = {
+    quemDeve: "Celso",
+    quemRecebe: "Amanda",
+    valor: Math.abs(saldo)
+  };
+}
+
+return {
+  saldoFinal,
+  detalhamento
+};
 }
